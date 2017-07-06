45c3eeaf0a14b596cdbf31f92e5205d11ca336cc
Chase Midler <cptmidler@sei.cmu.edu>
2017-07-06 14:29:34 -0400

    Added Java and Python projects and solved question 1 and 2 in chapter 1

---
+++ CTCI-Java/src/Chapter1/Question1.java
@@ -0,0 +1,42 @@
+package Chapter1;
+
+/**
+ * Created by cptmidler on 7/6/17.
+ */
+import java.util.HashMap;
+import java.util.Arrays;
+public class Question1 {
+    //O(n) space and time
+    boolean hasUCharacters(String s) {
+        HashMap h = new HashMap();
+        for(int i = 0; i<s.length(); i++)
+        {
+            char c = s.charAt(i);
+            if(h.containsKey(c))
+                return false;
+            h.put(c,1);
+        }
+        return true;
+    }
+
+    //assuming lowercase english letters only (26 values in 8 byte int)
+    boolean betterHasUCharacters(String s){
+        int check = 0;
+        for(int i =0; i<s.length(); i++)
+        {
+            int val = s.charAt(i) - 'a';
+            if((check & (1 <<val)) >0)
+                return false;
+            check |=(1<<val);
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Question1().hasUCharacters("test"));
+        System.out.println(new Question1().hasUCharacters("true"));
+        System.out.println(new Question1().betterHasUCharacters("test"));
+        System.out.println(new Question1().betterHasUCharacters("true"));
+    }
+}
---
+++ CTCI-Python/Chapter1/Question2.py
@@ -0,0 +1,26 @@
+def isPermutation(s1,s2):
+	if (len(s1) != len(s2)):
+		return False
+
+	d1 = {}
+	d2 = {}
+
+	for c in s1:
+		if c in d1:
+			d1[c]+=1
+		else:
+			d1[c] = 1
+	for c in s2:
+		if c in d2:
+			d2[c]+=1
+		else:
+			d2[c] = 1
+
+	for k in d1.keys():
+		if k not in d2 or d1[k] != d2[k]:
+			return False
+	return True
+
+print isPermutation("test", "sett")
+print isPermutation("tests", "sett")
+print isPermutation("tests", "eswtt")
