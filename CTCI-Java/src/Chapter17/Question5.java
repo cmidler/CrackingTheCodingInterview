67b0a98c97f90d946696ac966592dc4d1a247a7a
Chase Midler <cptmidler@sei.cmu.edu>
2017-07-28 09:20:15 -0400

    Doing a new commit

--- CTCI-Java/src/Chapter17/Question5.java
+++ CTCI-Java/src/Chapter17/Question5.java
@@ -2,6 +2,12 @@ package Chapter17;
 
 /**
  * Created by cptmidler on 7/18/17.
+ * Letters and Numbers: Given an array filled with letters and numbers, find the longest subarray with an equal number
+ * of letters and numbers.
+
  */
 public class Question5 {
+
+
+
 }
